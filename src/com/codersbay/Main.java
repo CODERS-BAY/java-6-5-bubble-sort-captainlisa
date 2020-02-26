0364ccc506d0d8c0718a7fed5b4a69f7828023cc
Lisa-Maria Plakolb <lisa.plakolb96@gmx.at>
2020-02-26 10:40:25 +0100

    Add Bubble Sort

--- src/com/codersbay/Main.java
+++ src/com/codersbay/Main.java
@@ -1,4 +1,24 @@
 package com.codersbay;
 
+import java.util.Arrays;
+
 public class Main {
+
+    public static void main(String[] args) {
+
+        int[] numbers = {4, 76, -33, 18, 0, -5};
+        System.out.println(Arrays.toString(numbers));
+        int temp = 0;
+
+        for (int i = 0; i < numbers.length - 1; i++) {
+            for (int j = 0; j < numbers.length - 1 - i; j++) {
+                if (numbers[j] > numbers[j + 1]) {
+                    temp = numbers[j];
+                    numbers[j] = numbers[j + 1];
+                    numbers[j + 1] = temp;
+                }
+            }
+            System.out.println(Arrays.toString(numbers));
+        }
+    }
 }
